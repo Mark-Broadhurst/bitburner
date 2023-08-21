aa9a88ecc1d8c83b37f203c9d92e6310500f641e
Mark Broadhurst <Mark.Broadhurst@gmail.com>
2023-08-21 22:56:21 +0100

    Added SubArrayWithMaxSum

--- codecontract/generate.js
+++ codecontract/generate.js
@@ -6,8 +6,11 @@ export async function main(ns) {
         ns.rm(file);
     });
 
+    ns.codingcontract.createDummyContract("Total Ways to Sum");
+
+    /*
     ns.codingcontract.getContractTypes().forEach(type => {
         ns.codingcontract.createDummyContract(type);
     });
-
+*/
 }
--- codecontract/subArrayWithMaximumSum.js
+++ codecontract/subArrayWithMaximumSum.js
@@ -5,6 +5,22 @@ export async function main(ns) {
   const hostname = ns.args[0];
   const filename = ns.args[1];
   const data = ns.codingcontract.getData(filename, hostname);
-  
-  ns.print(ns.codingcontract.getDescription(filename, hostname));
+  const answer = subArrayWithMaximumSum(data);
+  const result = ns.codingcontract.attempt(answer,filename,hostname);
+  ns.toast(`Solving Code Contract on ${hostname}:${filename} ${result}`)
+}
+
+function subArrayWithMaximumSum(data) {
+  let max = 0;
+  let sum = 0;
+  for (let i = 0; i < data.length; i++) {
+    sum += data[i];
+    if (sum < 0) {
+      sum = 0;
+    }
+    if (sum > max) {
+      max = sum;
+    }
+  }
+  return max;
 }
