b88a4dc2637aa6b0e6a863067016f1d45bb9dd23
Mark Broadhurst <Mark.Broadhurst@gmail.com>
2023-08-21 21:42:02 +0100

    Added Spiralise Matrix

--- codecontract/spiralizeMatrix.js
+++ codecontract/spiralizeMatrix.js
@@ -7,5 +7,40 @@ export async function main(ns) {
     const filename = ns.args[1];
     const data = ns.codingcontract.getData(filename, hostname);
         
+    const result = spiralizeMatrix(data);
     ns.toast(`Solving Code Contract on ${hostname}:${filename} ${result}`)
+}
+
+function spiralizeMatrix(matrix) {
+    const result = [];
+    let top = 0;
+    let bottom = matrix.length - 1;
+    let left = 0;
+    let right = matrix[0].length - 1;
+    let direction = 0;
+    while (top <= bottom && left <= right) {
+        if (direction === 0) {
+            for (let i = left; i <= right; i++) {
+                result.push(matrix[top][i]);
+            }
+            top++;
+        } else if (direction === 1) {
+            for (let i = top; i <= bottom; i++) {
+                result.push(matrix[i][right]);
+            }
+            right--;
+        } else if (direction === 2) {
+            for (let i = right; i >= left; i--) {
+                result.push(matrix[bottom][i]);
+            }
+            bottom--;
+        } else if (direction === 3) {
+            for (let i = bottom; i >= top; i--) {
+                result.push(matrix[i][left]);
+            }
+            left++;
+        }
+        direction = (direction + 1) % 4;
+    }
+    return result;
 }
